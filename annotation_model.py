2adf0f33ceaabde88dbcbc707bdd728d28d492a7
Nei Neto <nei.neto@hotmail.com>
2019-10-09 15:48:45 -0300

    Log timestamps when translators start and finish a sentence

--- annotation_model.py
+++ annotation_model.py
@@ -1,5 +1,6 @@
 
-from typing import List
+from datetime import datetime
+from typing import List, Tuple
 from pathlib import Path
 
 from mypy_extensions import TypedDict
@@ -10,6 +11,7 @@ class Translation(TypedDict):
     markers: List[str]
     original_sentence: str
     translated_sentences: List[str]
+    timestamps: List[Tuple[datetime, datetime]]
 
 
 def save_annotation(
@@ -19,22 +21,25 @@ def save_annotation(
     translated_sentence: str,
     marker: str,
     folder: Path,
+    timestamps: Tuple[datetime, datetime],
 ):
     obj = Translation(
         markers=[marker],
         original_sentence=original_sentence,
         translated_sentences=[translated_sentence],
+        timestamps=[timestamps]
     )
     translation_file: Path = folder / str(image_id) / f"{caption_id}.json"
     if translation_file.is_file():
         data = Translation(**json.load(str(translation_file), verbose=False))
         data["markers"].append(marker)
         data["translated_sentences"].append(translated_sentence)
+        data["timestamps"].append(timestamps)
         obj = data
     translation_file.parent.mkdir(parents=True, exist_ok=True)
     json.dump(obj, str(translation_file), indent=2, overwrite=True, verbose=False)
     print(f"Saved img {image_id}, cap {caption_id}.")
-    
+
 
 def load_annotation(
     image_id: int,
--- utils.py
+++ utils.py
@@ -1,8 +1,20 @@
 
+from datetime import datetime
 from typing import Tuple
 
 import numpy as np
+import pytz
 from matplotlib import pyplot as plt
+
+
+LOCAL_TIMEZONE = pytz.timezone("America/Sao_Paulo")
+
+
+def get_current_datetime():
+    right_now = datetime.now()
+    right_now = right_now.replace(tzinfo=pytz.UTC)
+    right_now = right_now.astimezone(LOCAL_TIMEZONE)
+    return right_now
 
 
 def show_image(image: np.ndarray, figure_size: Tuple[int, int] = (8, 8)):
